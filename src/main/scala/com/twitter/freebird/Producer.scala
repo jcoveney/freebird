c4d250c11fa8849ceab5acb57666c16a87caf90b
Jonathan Coveney <jcoveney@twitter.com>
2014-06-12 20:51:59 -0700

    Initial work

--- src/main/scala/com/twitter/freebird/Producer.scala
+++ src/main/scala/com/twitter/freebird/Producer.scala
@@ -6,14 +6,14 @@ object Producer {
 
 // This is something operable
 sealed trait Producer[P <: Platform[P], T] {
-  def map[U](fn: T => U): Producer[P, U] = MapProducer(this, fn)
+  def map[U](fn: T => U): Producer[P, U] = Map(this, fn)
 
-  def flatMap[U](fn: T => TraversableOnce[U]): Producer[P, U] = FlatMapProducer(this, fn)
+  def flatMap[U](fn: T => TraversableOnce[U]): Producer[P, U] = FlatMap(this, fn)
 
-  def filter(fn: T => Boolean): Producer[P, U] = FilterProducer(this, fn)
+  def filter(fn: T => Boolean): Producer[P, U] = Filter(this, fn)
 
   def group[K, V](implicit ev: T <:< (K, V)): KeyedProducer[P, K, V] =
-    GroupProducer(this.asInstanceOf[Producer[P, (K, V)]])
+    Group(this.asInstanceOf[Producer[P, (K, V)]])
 
   def groupBy[K](fn: T => K): KeyedProducer[P, K, T] = group(map { t => (fn(t), t)})
 
@@ -29,22 +29,22 @@ sealed trait KeyedProducer[P <: Platform[P], K, V] extends Producer[P, (K, V)] {
 //TODO can we make this another trait that isn't a KeyedProducer?
 //TODO can we use the trick scalding did to make it so that they can only sort once?
 sealed trait SortedKeyedProducer[P <: FreePlatform, K, V] extends KeyedProducer[P, K, V] {
-  def fold[U](init: U)(fn: (U, V) => U): KeyedProducer[P, K, U] =
+  def fold[U](init: U)(fn: (U, V) => U): Producer[P, (K, U)] = Fold(this, init, fn)
 }
 
 case class Source[P <: StreamPlatform[P], T](source: P#Source[T])
   extends Producer[P, T]
 
-case class MapProducer[P <: StreamPlatform[P], T, U](parent: Producer[P, T], fn: T => U)
+case class Map[P <: StreamPlatform[P], T, U](parent: Producer[P, T], fn: T => U)
   extends Producer[P, U]
 
-case class FlatMapProducer[P <: StreamPlatform[P], T, U](parent: Producer[P, T], fn: T => TraversableOnce[U])
+case class FlatMap[P <: StreamPlatform[P], T, U](parent: Producer[P, T], fn: T => TraversableOnce[U])
   extends Producer[P, U]
 
-case class FilterProducer[P <: StreamPlatform[P], T](parent: Producer[P, T], fn: T => Boolean)
+case class Filter[P <: StreamPlatform[P], T](parent: Producer[P, T], fn: T => Boolean)
   extends Producer[P, T]
 
-case class GroupProducer[P <: StreamPlatform[P], K, V](parent: Producer[P, (K, V)])
+case class Group[P <: StreamPlatform[P], K, V](parent: Producer[P, (K, V)])
   extends KeyedProducer[P, K, V]
 
 case class Summer[P <: StreamPlatform[P], K, V](parent: KeyedProducer[P, K, V], semi: Semigroup[V])
@@ -54,5 +54,5 @@ case class Sorted[P <: FreePlatform[P], K, V](parent: KeyedProducer[P, K, V], or
   extends SortedKeyedProducer[P, K, V]
 
 //TODO I don't like the need for a separate Fold and SortedFold
-case class SortedFold[P <: FreePlatform[P], K, V, U](parent: SortedKeyedProducer[P, K, V], init: U, fn: (U, V) => U)
-  extends KeyedProducer[P, K, U]
+case class Fold[P <: FreePlatform[P], K, V, U](parent: SortedKeyedProducer[P, K, V], init: U, fn: (U, V) => U)
+  extends Producer[P, (K, U)]
