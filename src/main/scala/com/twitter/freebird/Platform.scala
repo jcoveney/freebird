d8581329a666b3cd15103c4ec4ad5ff2c756c9e5
Jonathan Coveney <jcoveney@twitter.com>
2014-06-14 02:40:50 -0700

    refactoring...

--- src/main/scala/com/twitter/freebird/Platform.scala
+++ src/main/scala/com/twitter/freebird/Platform.scala
@@ -10,8 +10,10 @@ trait StreamPlatform[P <: StreamPlatform[P]] {
   type Store[_]
   type Plan[_]
 
-  def plan[T, S <: PlannableState](p: Producer[P, S, T]): Plan[T]
-  def run[T](plan: Plan[T]): Unit
+  def plan[T, S <: PlannableState](p: Producer[MemoryPlatform, S, Id[T]]): Plan[T]
+
+  //TODO can we avoid the id/keyed explosion?
+  //def run[T](plan: Plan[T]): Unit
 }
 
 import collection.mutable.{ Buffer, Map => MMap }
@@ -19,30 +21,52 @@ import collection.mutable.{ Buffer, Map => MMap }
 class MemoryPlatform extends FreePlatform[MemoryPlatform] {
   type Source[T] = List[T]
   type Store[T] = Buffer[T]
+
   type Plan[T] = MemoryPhysical[T]
 
-  private type MProducer[S <: State, T] = Producer[MemoryPlatform, S, T]
+  private type MProducer[S <: State, R <: Ret] = Producer[MemoryPlatform, S, R]
 
-  private[this] def inPlan[T, S <: State](p: MProducer[S, T]): MemoryPhysical[T] =
+  private[this] def optionFun2ConcatFun[U, T](fn: U => Option[T]): U => TraversableOnce[T] = fn(_).toList
+
+  private[this] def map2ConcatFun[U, T](fn: U => T): U => TraversableOnce[T] = { v => List(fn(v)) }
+
+  private[this] def keyGrabbingFun[K, V](fn: V => K): V => (K, V) = { v => (fn(v), v)}
+
+  //TODO we need a way to map from the T in the Producer to the T in MemoryPhysical.
+  // we can either leak something into the Producer, that is, the effective type
+  // in the underlying, but maintain type safety, or we can try and figure something else out.
+  // First try to figure something out, as we want this to be abstract.
+
+  //TODO might need a special one for the keyed ones?
+  private[this] def idInPlan[T, S <: State](p: MProducer[S, Id[T]]): MemoryPhysical[T] =
     p match {
       case Source(source) => SourceMP(source)
-      case Map(parent, fn) => throw new UnsupportedOperationException("implement")
-      case OptionMap(parent, fn) => throw new UnsupportedOperationException("implement")
-      case ConcatMap(parent, fn) => throw new UnsupportedOperationException("implement")
-      case Filter(parent, fn) => throw new UnsupportedOperationException("implement")
-      case Group(parent) => throw new UnsupportedOperationException("implement")
-      case GroupBy(parent, fn) => throw new UnsupportedOperationException("implement")
-      case GroupAll(parent) => throw new UnsupportedOperationException("implement")
+      case Map(parent, fn) => idInPlan(ConcatMap(parent, map2ConcatFun(fn)))
+      case OptionMap(parent, fn) => idInPlan(ConcatMap(parent, optionFun2ConcatFun(fn)))
+      case ConcatMap(parent, fn) => ConcatMapMP(Some(idInPlan(parent)), fn, Nil)
+      case Filter(parent, fn) => idInPlan(ConcatMap(parent, { v: T => if (fn(v)) List(v) else Nil }))
+      case Store(parent, store) => StoreMP(idInPlan(parent), store)
+      case Name(parent, name) => idInPlan(parent)
+    }
+
+  private[this] def keyedInPlan[K, V, S <: State](p: MProducer[S, Keyed[K, V]]): MemoryPhysical[(K, Seq[V])] =
+    p match {
+      // TODO we need to deal with the synthetic keyed type.
+      // Perhaps we can make the T in producer be a type encapsulating return values?
+      // Or perhaps it is enough to have inPlan have an ev parameter for the applicable ones?
+      case Group(parent) => GroupMP(idInPlan(parent))
+      case GroupBy(parent, fn) => keyedInPlan(Group(Map(parent, keyGrabbingFun(fn))))
+      case GroupAll(parent) => keyedInPlan[Unit, V, S](GroupBy(parent, { _ => Unit }))
       case Reducer(parent, fn) => throw new UnsupportedOperationException("implement")
       case Sorted(parent, ord) => throw new UnsupportedOperationException("implement")
       case Fold(parent, init, fn) => throw new UnsupportedOperationException("implement")
-      case Store(parent, store) => StoreMP(inPlan(parent), store)
-      case Name(parent, name) => inPlan(parent)
+      case Name(parent, name) => keyedInPlan(parent)
     }
 
-  override def plan[T, S <: PlannableState](p: MProducer[S, T]): MemoryPhysical[T] = inPlan(p)
+  //TODO as with store, I don't like that this force Id[T]
+  override def plan[T, S <: PlannableState](p: Producer[MemoryPlatform, S, Id[T]]) = idInPlan(p)
 
-  override def run[T](plan: MemoryPhysical[T]) {
+  /*override*/ def run[T](plan: MemoryPhysical[T]) {
     @annotation.tailrec
     def runOn[U](p: MemoryPhysical[U]) {
       val (t, newp) = p.getNext
@@ -58,6 +82,7 @@ sealed trait MemoryPhysical[T] {
   def getNext: (T, Option[MemoryPhysical[T]])
 }
 case class SourceMP[T](input: List[T]) extends MemoryPhysical[T] {
+  require(input.nonEmpty, "Source must have input. Otherwise don't create one.")
   override def getNext = input match {
     case head :: Nil => (head, None)
     case head :: tail => (head, Some(SourceMP(tail)))
@@ -70,11 +95,33 @@ case class StoreMP[T](p: MemoryPhysical[T], buf: Buffer[T]) extends MemoryPhysic
     (v, newp.map(StoreMP(_, buf)))
   }
 }
-case class FlatMapMP[T, U](p: MemoryPhysical[T], fn: T => TraversableOnce[U], rem: List[U] = Nil) extends MemoryPhysical[U] {
+case class ConcatMapMP[T, U](p: Option[MemoryPhysical[T]], fn: T => TraversableOnce[U], rem: List[U])
+    extends MemoryPhysical[U] {
   override def getNext =
     rem match {
+      case Nil =>
+        val (v, newp) = p.getOrElse(throw new IllegalStateException("Nothing from source")).getNext
+        val newv = fn(v).toList
+        if (newv.isEmpty) {
+          ConcatMapMP(newp, fn, Nil).getNext
+        } else {
+          val head = newv.head
+          val tail = newv.tail
+          if (newp.isEmpty && tail.isEmpty) {
+            (head, None)
+          } else {
+            (head, Some(ConcatMapMP(newp, fn, tail)))
+          }
+        }
       case head :: Nil =>
-
-      case head :: tail => (v, FlatMapMP(p, fn, tail))
+        (head, p.map { par =>
+          val (newv, newp) = par.getNext
+          ConcatMapMP(newp, fn, fn(newv).toList)
+        })
+      case head :: tail => (head, Some(ConcatMapMP(p, fn, tail)))
     }
+}
+
+case class GroupMP[K, V](p: MemoryPhysical[(K, V)]) extends MemoryPhysical[(K, Seq[V])] {
+  override def getNext = throw new UnsupportedOperationException("type checks!")
 }
--- src/main/scala/com/twitter/freebird/Producer.scala
+++ src/main/scala/com/twitter/freebird/Producer.scala
@@ -4,9 +4,10 @@ package com.twitter.freebird
 // the number of classes
 
 object Producer {
-  def source[P <: StreamPlatform[P], T](s: P#Source[T]): Producer[P, NoState, T] = Source[P, T](s)
+  def source[P <: StreamPlatform[P], T](s: P#Source[T]): Producer[P, NoState, Id[T]] = Source[P, T](s)
 }
 
+// State machine for storage etc
 sealed trait State
 sealed trait NonPlannableState extends State
 sealed trait NoState extends NonPlannableState
@@ -15,63 +16,92 @@ sealed trait SortedState extends NonPlannableState
 sealed trait PlannableState extends State
 sealed trait StoreState extends PlannableState
 
-sealed trait Keyed[K, V]
+// State machine for type states, necessary to separate logical details from physical
 
 // This is something operable
 sealed trait Producer[P <: StreamPlatform[P], S <: State, T] {
-  def name(str: String): Producer[P, S, T] = Name(this, str)
+  def name(str: String): Producer[P, S, R] = Name(this, str)
 
-  def map[U](fn: T => U): Producer[P, NoState, U] = Map(this, fn)
+  def map[T, U](fn: T => U)(implicit ev: R <:< Id[T]): Producer[P, NoState, Id[U]] =
+    Map(this.asInstanceOf[Producer[P, S, Id[T]]], fn)
 
-  def optionMap[U](fn: T => Option[U]): Producer[P, NoState, U] = OptionMap(this, fn)
+  def optionMap[T, U](fn: T => Option[U])(implicit ev: R <:< Id[T]): Producer[P, NoState, Id[U]] =
+    OptionMap(this.asInstanceOf[Producer[P, S, Id[T]]], fn)
 
-  def concatMap[U](fn: T => TraversableOnce[U]): Producer[P, NoState, U] = ConcatMap(this, fn)
+  def concatMap[T, U](fn: T => TraversableOnce[U])(implicit ev: R <:< Id[T]): Producer[P, NoState, Id[U]] =
+    ConcatMap(this.asInstanceOf[Producer[P, S, Id[T]]], fn)
 
-  def filter(fn: T => Boolean): Producer[P, NoState, T] = Filter(this, fn)
+  def filter[T](fn: T => Boolean)(implicit ev: R <:< Id[T]): Producer[P, NoState, Id[T]] =
+    Filter(this.asInstanceOf[Producer[P, S, Id[T]]], fn)
 
-  def group[K, V](implicit ev: T <:< (K, V)): Producer[P, NoState, Keyed[K, V]] =
-    Group(this.asInstanceOf[Producer[P, S, (K, V)]])
+  def group[K, V](implicit ev: R <:< Id[(K, V)]): Producer[P, NoState, Keyed[K, V]] =
+    Group(this.asInstanceOf[Producer[P, S, Id[(K, V)]]])
 
-  def groupBy[K](fn: T => K): Producer[P, NoState, Keyed[K, T]] = GroupBy(this, fn)
+  def groupBy[K, V](fn: V => K)(implicit ev: R <:< Id[V]): Producer[P, NoState, Keyed[K, V]] =
+    GroupBy(this.asInstanceOf[Producer[P, S, Id[V]]], fn)
 
-  def groupAll: Producer[P, NoState, Keyed[Unit, T]] = GroupAll(this)
+  // ****
+  // ****
+  //TODO the explosion of Id/Keyed makes it seem pretty clear that it is useful to have
+  // 2 traits like we originally did. Need to think about how to get between them.
+  // ****
+  // ****
+  def groupAll[T](implicit ev: R <:< Id[T]): Producer[P, NoState, Keyed[Unit, T]] =
+    GroupAll(this.asInstanceOf[Producer[P, S, Id[T]]])
 
-  // Should return itself, right? Might be too hard to do...
-  def write(store: P#Store[T]): Producer[P, StoreState, T] = Store(this, store)
+  //TODO is this necessary? Doesn't seem like it...
+  def write[T](store: P#Store[T])(implicit ev: R <:< Id[T]): Producer[P, StoreState, Id[T]] =
+    Store(this.asInstanceOf[Producer[P, S, Id[T]]], store)
 
-  def reduceByKey[K, V](fn: (V, V) => V)(implicit ev: T <:< Keyed[K, V]): Producer[P, NoState, Keyed[K, V]] =
+  // TODO what does it mean to write right after a reduceByKey?
+  def reduceByKey[K, V](fn: (V, V) => V)(implicit ev: R <:< Keyed[K, V]): Producer[P, NoState, Keyed[K, V]] =
     Reducer(this.asInstanceOf[Producer[P, S, Keyed[K, V]]], fn)
 
-  def sort[NewP <: FreePlatform[NewP], K, V](implicit ord: Ordering[V], ev1: P <:< NewP, ev2: T <:< Keyed[K, V]): Producer[NewP, SortedState, Keyed[K, V]] =
+  def sort[NewP <: FreePlatform[NewP], K, V](implicit ord: Ordering[V], ev1: P <:< NewP, ev2: R <:< Keyed[K, V]): Producer[NewP, SortedState, Keyed[K, V]] =
     Sorted(this.asInstanceOf[Producer[NewP, SortedState, Keyed[K, V]]], ord)
 
   def fold[NewP <: FreePlatform[NewP], K, V, U](init: U)(fn: (U, V) => U)
-      (implicit ev1: P <:< NewP, ev2: S <:< SortedState, ev3: T <:< Keyed[K, V]): Producer[NewP, NoState, Keyed[K, U]] =
+      (implicit ev1: P <:< NewP, ev2: S <:< SortedState, ev3: R <:< Keyed[K, V]): Producer[NewP, NoState, Keyed[K, U]] =
     Fold(this.asInstanceOf[Producer[NewP, SortedState, Keyed[K, V]]], init, fn)
 }
 
+//I propose the following...
+sealed trait Producer[P <: StreamPlatform[P], S <: State, T, This <: Producer[P, S, T, This]] {
+  def name(str: String): This = Name(this, str)
+  def write(store: P#Store[T]): This = Store(this, store)
+}
+
+sealed trait UnkeyedProducer[P <: StreamPlatform[P], S <: State, T]
+  extends Producer[P, S, T, UnkeyedProducer[P, S, T]]
+  
+sealed trait KeyedProducer[P <: StreamPlatform[P], S <: State, K, V]
+  extends Producer[P, S, (K, V), KeyedProducer[P, S, K, V]]
+
+// AHA! This is the link! since it is a platform, we are golden. we will physically manifest (K, V)'s
+sealed trait KeyedProducer[P <: StreamPlatform[P], S <: State, K, V] extends Platform[P, S, (K, V)]
+
 case class Source[P <: StreamPlatform[P], T](source: P#Source[T])
-  extends Producer[P, NoState, T]
+  extends Producer[P, NoState, Id[T]]
 
-case class Map[P <: StreamPlatform[P], T, U](parent: Producer[P, _ <: State, T], fn: T => U)
-  extends Producer[P, NoState, U]
+case class Map[P <: StreamPlatform[P], T, U](parent: Producer[P, _ <: State, Id[T]], fn: T => U)
+  extends Producer[P, NoState, Id[U]]
 
-case class OptionMap[P <: StreamPlatform[P], T, U](parent: Producer[P, _ <: State, T], fn: T => Option[U])
-  extends Producer[P, NoState, U]
+case class OptionMap[P <: StreamPlatform[P], T, U](parent: Producer[P, _ <: State, Id[T]], fn: T => Option[U])
+  extends Producer[P, NoState, Id[U]]
 
-case class ConcatMap[P <: StreamPlatform[P], T, U](parent: Producer[P, _ <: State, T], fn: T => TraversableOnce[U])
-  extends Producer[P, NoState, U]
+case class ConcatMap[P <: StreamPlatform[P], T, U](parent: Producer[P, _ <: State, Id[T]], fn: T => TraversableOnce[U])
+  extends Producer[P, NoState, Id[U]]
 
-case class Filter[P <: StreamPlatform[P], T](parent: Producer[P, _ <: State, T], fn: T => Boolean)
-  extends Producer[P, NoState, T]
+case class Filter[P <: StreamPlatform[P], T](parent: Producer[P, _ <: State, Id[T]], fn: T => Boolean)
+  extends Producer[P, NoState, Id[T]]
 
-case class Group[P <: StreamPlatform[P], K, V](parent: Producer[P, _ <: State, (K, V)])
+case class Group[P <: StreamPlatform[P], K, V](parent: Producer[P, _ <: State, Id[(K, V)]])
   extends Producer[P, NoState, Keyed[K, V]]
 
-case class GroupBy[P <: StreamPlatform[P], K, V](parent: Producer[P, _ <: State, V], fn: V => K)
+case class GroupBy[P <: StreamPlatform[P], K, V](parent: Producer[P, _ <: State, Id[V]], fn: V => K)
   extends Producer[P, NoState, Keyed[K, V]]
 
-case class GroupAll[P <: StreamPlatform[P], V](parent: Producer[P, _ <: State, V])
+case class GroupAll[P <: StreamPlatform[P], V](parent: Producer[P, _ <: State, Id[V]])
   extends Producer[P, NoState, Keyed[Unit, V]]
 
 case class Reducer[P <: StreamPlatform[P], K, V](parent: Producer[P, _ <: State, Keyed[K, V]], fn: (V, V) => V)
@@ -83,8 +113,8 @@ case class Sorted[P <: FreePlatform[P], K, V](parent: Producer[P, _ <: State, Ke
 case class Fold[P <: FreePlatform[P], K, V, U](parent: Producer[P, SortedState, Keyed[K, V]], init: U, fn: (U, V) => U)
   extends Producer[P, NoState, Keyed[K, U]]
 
-case class Store[P <: StreamPlatform[P], T](parent: Producer[P, _ <: State, T], store: P#Store[T])
-  extends Producer[P, StoreState, T]
+case class Store[P <: StreamPlatform[P], T](parent: Producer[P, _ <: State, Id[T]], store: P#Store[T])
+  extends Producer[P, StoreState, Id[T]]
 
-case class Name[P <: StreamPlatform[P], S <: State, T](parent: Producer[P, S, T], name: String)
-  extends Producer[P, S, T]
+case class Name[P <: StreamPlatform[P], S <: State, R <: Ret](parent: Producer[P, S, R], name: String)
+  extends Producer[P, S, R]
